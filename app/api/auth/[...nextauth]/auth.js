1654a9de8d474842e25874ea35e5a5f12e2919ff
alexisj890 <alexjuarez4002@gmail.com>
2025-12-13 22:20:05 -0500

    Major changes on the home page (#26)
    * Rename footer.jsx to Footer.jsx
    
    * Fix import path for Footer component
    
    * Implement NextAuth with Google, GitHub, and Discord
    
    * Refactor NextAuth configuration into separate file
    
    * Fixed the landing page and did some much needed changes
    
    ---------
    
    Co-authored-by: Hamim <hamimc232@gmail.com>

---
+++ app/api/auth/[...nextauth]/auth.js
@@ -0,0 +1,48 @@
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+import { supabaseAdmin } from "@/lib/supabaseAdmin";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub;
+      return session;
+    },
+    async redirect({ baseUrl }) {
+      return `${baseUrl}/`;
+    },
+  },
+  events: {
+    async signIn({ user, account }) {
+      await supabaseAdmin.from("users").upsert(
+        {
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          email: user.email,
+          name: user.name,
+          image: user.image,
+          updated_at: new Date(),
+        },
+        {
+          onConflict: "provider_account_id",
+        }
+      );
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};

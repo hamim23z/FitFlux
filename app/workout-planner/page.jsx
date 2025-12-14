cc23690d858e711977f875349c8c224cf54e3b0d
alexisj890 <alexjuarez4002@gmail.com>
2025-12-14 02:21:13 -0500

    User setting page. this page allows user to change infor mation. the page is fully done

--- app/workout-planner/page.jsx
+++ app/workout-planner/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { supabase } from "../../lib/supabaseClient";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import ExerciseList from "../components/ExerciseList";
 import ExerciseDetails from "../components/ExerciseDetails";
 import AISuggestions from "../components/AISuggestions";
